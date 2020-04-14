dc788f98e6dcf93edbf4411377973ddf2e0c47bf
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-13 22:44:21 -0400

    Update footer.php

--- Projects/Hotel/footer.php
+++ Projects/Hotel/footer.php
@@ -4,9 +4,8 @@
         <a href="#">Contact Us</a>
         <a href="http://www.google.com/maps/?q=40.7556307, -73.9789062">Directions</a>
         <p><a href="#">465 West 47th Street, New York, 10018</a></p>
-        <!--<p><u>Address</u>: <a href="#">465 West 47th Street, New York, 10018</a></p>-->
-
     </div>
+    
     <div id="footerRight">
         <a href="#"><img src ="images/facebook.png" alt="Facebook"></a>
         <a href="#"><img src ="images/instagram.png" alt ="Instagram"></a>
