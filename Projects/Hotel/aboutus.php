3070daa5e9b2a2385bace9d8b184df987872b259
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-13 22:41:14 -0400

    Update aboutus.php

--- Projects/Hotel/aboutus.php
+++ Projects/Hotel/aboutus.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Sweet Home - About Us</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="Home Sweeet Home About Us Page">
+    <meta name="author" content="Leslie Nertomb">
+    <link rel="stylesheet" href="css/aboutus.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
 
+<body>
+<?php
+include('header.php')
+?>
+
+<section>
+    <div id="body">
+        <div id="bio">
+        <h1>Biography</h1>
+        <img src ="images/bio.jpg" alt="Bio Picture">
+        <p>Hey I'm Leslie, the founder of Home Sweet Home. </p>
+            <p>I’m obsessed with exploring the world, meeting amazing people and
+            getting as lost as possible with my camera. I was on the road for 1467 days between 2014 – 2018,
+            and the best places I visited were the ones where I felt like home.</p>
+         <p>Currently living in New York, I want to make you feel the same. That's why I am taking a chance on
+             changing everyone's view on hotel stays. Somehow it's all working out.</p>
+            <p>&nbsp</p>
+            <p>&nbsp</p>
+            <p>&nbsp</p>
+        </div>
+
+        <div id="mywork">
+        <h1>My Home Sweet Home Project</h1>
+            <p>I built this project from scratch. I acted as a:
+                <ul>
+                <li>Project Manager</li>
+                <li>Database Specialist</li>
+                <li>Front-End Developer</li>
+                <li>Back-End Developer</li>
+                <ul>
+            </p>
+
+        </div>
+    </div>
+</section>
+
+<?php
+include('footer.php')
+?>
+</body>
+</html>
