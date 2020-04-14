9e90d96181839290b1581ca98333931bc824b296
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-13 22:43:48 -0400

    Create footer.php

---
+++ Projects/Hotel/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div id="footerLeft">
+        <p>&copy; 2019 Home Sweet Home</p>
+        <a href="#">Contact Us</a>
+        <a href="http://www.google.com/maps/?q=40.7556307, -73.9789062">Directions</a>
+        <p><a href="#">465 West 47th Street, New York, 10018</a></p>
+        <!--<p><u>Address</u>: <a href="#">465 West 47th Street, New York, 10018</a></p>-->
+
+    </div>
+    <div id="footerRight">
+        <a href="#"><img src ="images/facebook.png" alt="Facebook"></a>
+        <a href="#"><img src ="images/instagram.png" alt ="Instagram"></a>
+        <a href="#"><img src ="images/twitter.png" alt ="Twitter"></a>
+        <a href="#"><img src ="images/youtube.png" alt ="Youtube"></a>
+    </div>
+</footer>
