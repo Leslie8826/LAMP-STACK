0005d15de18eaf8b61ba7733acbc7bde2b4230bb
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-13 22:47:21 -0400

    Create header.php

---
+++ Projects/Hotel/header.php
@@ -0,0 +1,18 @@
+
+<header>
+    <div id="leftHeader">
+        <a href="welcome.php">
+            <img src="images/hSh_logo.png" alt ="hSh logo"/>
+        </a>
+    </div>
+
+    <div id="midHeader">
+        <div id="headStyle">Home Sweet Home &nbsp &nbsp &nbsp</div>
+        <div ><a href="#">465 West 47th Street, New York, 10018</a></div>
+
+    </div>
+
+    <div id="rightHeader">
+        <div class="bookingButton_room"><a href="reservation.php">BOOK NOW</a></div>
+    </div>
+</header>
