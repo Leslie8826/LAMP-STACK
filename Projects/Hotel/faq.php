e18753541c79c6c2a3b07033685b207b6d0c5c16
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-13 22:43:13 -0400

    Create faq.php

---
+++ Projects/Hotel/faq.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>The Home Sweet Home Hotel</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="css/faq.css"/>
+</head>
+
+<body>
+<?php
+include('header.php')
+?>
+
+<main>
+<section>
+    <div class="faq_container">
+        <div class="question">
+        <h1> FAQ </h1>
+        <h2> Does the hotel provide free WiFi? </h2>
+        <p>Premium high-speed internet access for all devices is included in The Park West Residence Fee
+            We provide complimentary WiFi throughout the entire building’s public spaces</p>
+        </div>
+
+        <div class="question">
+            <h2> Do you have a business center? </h2>
+            <p> Yes we do have a business center on the ground floor. </p>
+        </div>
+
+        <div class="question">
+            <h2> Billing Questions? Reservation Questions? Need a Copy of Your Bill? </h2>
+            <p> Please Email CustomerService@HomeSweetHome.com or Call +1(212) 866-1880 for all Reservation &
+                Billing Questions, Requests, Needs. </p>
+        </div>
+
+        <div class="question">
+            <h2> Do you have a restaurant on site? Room Service? Bar or Lounge? </h2>
+            <p> PPresent Your Park West Hotel Room Key & Receive 10% Off Your Entire Meal
+                Demitasse Coffee & Tea</p>
+
+                <p> Download the GrubHub App for Hundreds of nearby food delivery options
+                GrubHub Mobile Food Ordering App </p>
+        </div>
+
+    </div>
+
+
+</section>
+    <?php
+    include('footer.php')
+    ?>
+</main>
+
+
+</body>
+</html>
